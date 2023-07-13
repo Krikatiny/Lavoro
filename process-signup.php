a063b1a3c46db0df22a5342154e86043a55e32a9
Krikatiny <damerdaner21@gmail.com>
2023-07-13 19:40:28 +0300

    Воно майже працює, якщо хто хоче, продивіться чому при Submit, коли реєструєшься воно не працює

--- process-signup.php
+++ process-signup.php
@@ -1,32 +1,65 @@
 <?php
 
-if (empty($_POST["name"])){
+if (empty($_POST["name"])) {
     die("Name is required");
 }
 
-if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-    die("Invalid email");
+if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
 }
 
-if (strlen($_POST["password"]) < 8){
-    die("Password must be at least 8 character");
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
 }
 
-if (!preg_match("/[a-z]/i", $_POST["password"])){
+if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
 
-if (!preg_match("/[0-9]/", $_POST["password"])){
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
 
 if ($_POST["password"] !== $_POST["password_commit"]) {
-    die("Password don't match");
+    die("Passwords must match");
 }
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__ . "/database.php";
 
-print_r($_POST);
-var_dump($password_hash);
+$sql = "INSERT INTO user (name, email, password_hash)
+        VALUES (?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",
+    $_POST["name"],
+    $_POST["email"],
+    $password_hash);
+
+if ($stmt->execute()) {
+
+    header("Location: signup-success.html");
+    exit;
+
+} else {
+
+    if ($mysqli->errno === 1062) {
+        die("email already taken");
+    } else {
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+}
+
+
+
+
+
+
+
+
